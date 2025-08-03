8284f136d1ecfc81f5895d588a79c8ca71470eeb
me-Sushil <girbahadurbk123@gmail.com>
2025-08-04 00:52:42 +0545

    part3 component added

--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -1,5 +1,6 @@
 import Part1 from "./Part1";
 import Part2 from "./Part2";
+import Part3 from "./Part3";
 const Content = (props) => {
   return (
     <div>
---
+++ part1/src/Part3.jsx
@@ -0,0 +1,9 @@
+const Part3=(props)=>{
+    return(
+        <div>
+            <p>{props.Part3} {props.exercises3}</p>
+        </div>
+    )
+}
+
+export default Part3;
