a853a61273c880760f1f6091f29d6f3c8e31172e
me-Sushil <girbahadurbk123@gmail.com>
2025-08-04 01:07:17 +0545

    solve upper lower case problem

--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -6,7 +6,7 @@ const Content = (props) => {
     <div>
         <Part1 part1={props.part1} exercises1={props.exercises1}/>
         <Part2 part2={props.part2} exercises2={props.exercises2}/>
-        <Part3 Part3={props.Part3} exercises3={props.exercises3}/>
+        <Part3 part3={props.part3} exercises3={props.exercises3}/>
     </div>
   );
 };
