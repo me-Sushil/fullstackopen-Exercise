7e10f139f0466cba5949046c2583faa91d4a264c
me-Sushil <girbahadurbk123@gmail.com>
2025-08-04 00:47:34 +0545

    part 1 component added

--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -1,15 +1,10 @@
+import Part1 from "./Part1";
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+        <Part1 part1={props.part1} exercises1={props.exercises1}/>
+        <Part2 part2={props.part2} exercises2={props.exercises2}/>
+        <Part3 />
     </div>
   );
 };
---
+++ part1/src/Part1.jsx
@@ -0,0 +1,8 @@
+const Part1=(props)=>{
+    return(
+        <div>
+            <p>{props.Part1} {props.exercises1}</p>
+        </div>
+    )
+}
+export default Part1;
