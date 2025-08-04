abe6aa5d07f120b444553d26b2af4348f947cc99
me-Sushil <girbahadurbk123@gmail.com>
2025-08-04 18:07:46 +0545

    delete header externel component and add component inside App  for 1.3 task

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,31 +1,26 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
-
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const courseDetail = {
+    course: "Half Stack application development",
+    parts: [
+      { part1: "Fundamentals of React", exercises1: 10 },
+      { part2: "Using props to pass data", exercises2: 7 },
+      { part3: "State of a component", exercises3: 14 },
+    ],
+  };
+
+  const Header = (props) => {
+    return (
+      <>
+        <h1>{props.course}</h1>
+      </>
+    );
+  };
+
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Header course={courseDetail.course} />
+      <Content parts={courseDetail.parts} />
+      <Total parts={courseDetail.parts} />
     </div>
   );
 };
