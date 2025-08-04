906856e837fe2dc080fe7ced198a671a5c8f3c93
me-Sushil <girbahadurbk123@gmail.com>
2025-08-04 18:20:32 +0545

    delete total externel component and  add component inside App for 1.3 task

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -26,6 +26,14 @@ const App = () => {
     )
   }
 
+  const Total=(props)=>{
+    return(
+      <>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      </>
+    )
+  }
+
   return (
     <div>
       <Header course={courseDetail.course} />
