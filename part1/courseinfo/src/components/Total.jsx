64bda3d067e1898394baa9d79717cf1b4858d792
me-Sushil <girbahadurbk123@gmail.com>
2025-08-07 01:52:31 +0545

    Task 2.2: course information step7 completed

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,7 +1,9 @@
 const Total=({parts})=>{
+  let total =  parts.reduce((acc, part)=> acc+part.exercises,0);
     return(
         <>
-        
+        <p>total of {total} exercises</p>
+        {/* <p>total of {parts[0].exercises + parts[1].exercises + parts[2].exercises + parts[3].exercises} exercises</p> */}
         </>
     )
 }
