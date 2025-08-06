0f0f167b28892c50199ee57acbdbfa2ee64b84fd
me-Sushil <girbahadurbk123@gmail.com>
2025-08-06 11:30:23 +0545

    handle bad click

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,17 +15,21 @@ const App = () => {
     const newNeutral = neutral + 1;
     setNeutral(newNeutral);
   }
+  const handleBadClick=()=>{
+    const newBad = bad + 1;
+    setBad(newBad);
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
-      <button>bad</button>
+      <button onClick={handleBadClick}>bad</button>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
-      <p>bad</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
