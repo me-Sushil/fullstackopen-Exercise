1b5733c9feafbf1c69183011770cec2b2cea1971
me-Sushil <girbahadurbk123@gmail.com>
2025-08-07 01:31:16 +0545

    add object in array

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,30 +2,35 @@ import Course from "./components/Course";
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
-    id:1,
+    name: "Half Stack application development",
+    id: 1,
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
-        id:1
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
-        id:2
+        id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
-        id:3
-      }
-    ]
-  }
+        id: 3,
+      },
+      { 
+        name: "redux", 
+        exercises: 11, 
+        id: 4 
+      },
+    ],
+  };
 
   return (
     <div>
-      <Course  course={course}/>
+      <Course course={course} />
     </div>
   );
 };
