79af3a60ea1b5ea04c427e9e6fde2e8768b4e613
me-Sushil <girbahadurbk123@gmail.com>
2025-08-07 01:26:27 +0545

    Task 2.1: course information step6 completed

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,23 +1,13 @@
+  import Part from "./Part";
   const Content = ({ parts }) => {
     return (
       <>
-      <p>{parts[0].name} {parts[0].exercises}</p>
-      <p>{parts[1].name} {parts[1].exercises}</p>
-      <p>{parts[1].name} {parts[1].exercises}</p>
-        {/* <Part parts={parts[0]} />
+        <Part parts={parts[0]} />
         <Part parts={parts[1]} />
-        <Part parts={parts[2]} /> */}
+        <Part parts={parts[2]} />
       </>
     );
   };
 
   export default Content;
-//   const Part = ({ parts }) => {
-//     return (
-//       <>
-//         <p>
-//           {parts.name} {parts.exercises}
-//         </p>
-//       </>
-//     );
-//   };
+
---
+++ part1/courseinfo/src/components/Part.jsx
@@ -0,0 +1,8 @@
+const Part=({parts})=>{
+    return(
+        <>
+        <p>{parts.name} {parts.exercises}</p>
+        </>
+    )
+}
+export default Part;
