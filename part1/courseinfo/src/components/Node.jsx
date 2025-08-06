1839e052ab53a52214038abd676073586820dad9
me-Sushil <girbahadurbk123@gmail.com>
2025-08-07 02:32:52 +0545

    Task 2.4: course information step9  completed

--- part1/courseinfo/src/components/Node.jsx
+++ part1/courseinfo/src/components/Node.jsx
@@ -1,7 +1,13 @@
-const Node = ()=>{
+import Part from "./Part";
+import Total from "./Total";
+
+const Node = ({node})=>{
     return(
         <>
-        
+        <h4>Node.js</h4>
+        <Part parts={node.parts[0]}/>
+        <Part parts={node.parts[1]}/>
+        <Total parts={node.parts}/>
         </>
     )
 }
