73d732de639523a34eabc0badf8855d81015df4e
me-Sushil <girbahadurbk123@gmail.com>
2025-08-07 01:58:48 +0545

    Task 2.2 Completed

--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -2,7 +2,7 @@ const Total=({parts})=>{
   let total =  parts.reduce((acc, part)=> acc+part.exercises,0);
     return(
         <>
-        <p>total of {total} exercises</p>
+        <p style={{fontWeight:"bold"}}>total of {total} exercises</p>
         {/* <p>total of {parts[0].exercises + parts[1].exercises + parts[2].exercises + parts[3].exercises} exercises</p> */}
         </>
     )
