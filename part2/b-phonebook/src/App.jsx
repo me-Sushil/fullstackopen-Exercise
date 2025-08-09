04ee8a2a9daabb293bb265532631b5309bdb9bc9
me-Sushil <girbahadurbk123@gmail.com>
2025-08-10 01:55:31 +0545

    add person form component

--- part2/b-phonebook/src/App.jsx
+++ part2/b-phonebook/src/App.jsx
@@ -1,29 +1,10 @@
 import { useState } from "react";
 import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
 
 const App = () => {
   const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (newName.trim() === "" && number.trim() === "") return;
-    if (persons.some((person) => person.name.toLowerCase() === newName.trim().toLowerCase())) {
-      window.alert(`${newName} already added to phonebook`);
-      setNewName("");
-    } else {
-      const newObj = {
-        id: persons.length + 1,
-        name: newName.trim(),
-        number: number.trim(),
-      };
-      setPersons(persons.concat(newObj));
-      setNewName("");
-      setNumber("");
-    }
-  }
-
-  const [number, setNumber] = useState("");
   const [search, setSearch] = useState("");
 
   const filteredPerson = search ? persons.filter((person)=>
@@ -35,30 +16,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter search={search} setSearch={setSearch}/>
       <h3>add a new</h3>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <p>
-            name:
-            <input
-              placeholder="type here"
-              value={newName}
-              onChange={(e) => setNewName(e.target.value)}
-            />
-          </p>
-          <p>
-            number:
-            <input
-              type="tel"
-              placeholder="+977 98....."
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
-            />
-          </p>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm persons={persons} setPersons={setPersons}/>
       <h2>Numbers</h2>
       {filteredPerson.map((person) => {
         return (
---
+++ part2/b-phonebook/src/components/PersonForm.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+const PersonForm=({persons, setPersons})=>{
+
+      const [number, setNumber] = useState("");
+        const [newName, setNewName] = useState("");
+
+      
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (newName.trim() === "" && number.trim() === "") return;
+    if (persons.some((person) => person.name.toLowerCase() === newName.trim().toLowerCase())) {
+      window.alert(`${newName} already added to phonebook`);
+      setNewName("");
+    } else {
+      const newObj = {
+        id: persons.length + 1,
+        name: newName.trim(),
+        number: number.trim(),
+      };
+      setPersons(persons.concat(newObj));
+      setNewName("");
+      setNumber("");
+    }
+  } 
+    return(
+        <>
+        <form onSubmit={handleSubmit}>
+        <div>
+          <p>
+            name:
+            <input
+              placeholder="type here"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+          </p>
+          <p>
+            number:
+            <input
+              type="tel"
+              placeholder="+977 98....."
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+            />
+          </p>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+        </>
+    )
+}
+
+export default PersonForm;
