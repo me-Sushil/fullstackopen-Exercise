c9ca402878ad4883049a01206e5aa13a8e11f3c4
me-Sushil <girbahadurbk123@gmail.com>
2025-08-10 01:25:18 +0545

    Task 2.9  completed

--- part2/b-phonebook/src/App.jsx
+++ part2/b-phonebook/src/App.jsx
@@ -2,38 +2,46 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas" }]);
-
   const [newName, setNewName] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
-    if(newName ==="" && number==="") return;
-    if (persons.some((person) => person.name === newName.trim())) {
+    if (newName.trim() === "" && number.trim() === "") return;
+    if (persons.some((person) => person.name.toLowerCase() === newName.trim().toLowerCase())) {
       window.alert(`${newName} already added to phonebook`);
       setNewName("");
     } else {
       const newObj = {
         id: persons.length + 1,
-        name: newName,
-        number: number,
+        name: newName.trim(),
+        number: number.trim(),
       };
       setPersons(persons.concat(newObj));
       setNewName("");
       setNumber("");
     }
   }
-  function handleInput(event) {
-    setNewName(event.target.value);
-  }
 
   const [number, setNumber] = useState("");
+  const [search, setSearch] = useState("");
 
-  const numberHandle = (event) => {
-    setNumber(event.target.value);
-  };
+  const filteredPerson = search ? persons.filter((person)=>
+    person.name.toLowerCase().includes(search.toLowerCase())
+): persons;
 
   return (
-    <div>
+    <>
       <h2>Phonebook</h2>
+      <p>
+        filter shown with
+        <input
+          placeholder="Search by name"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </p>
+
+      <h3>add a new</h3>
       <form onSubmit={handleSubmit}>
         <div>
           <p>
@@ -41,7 +49,7 @@ const App = () => {
             <input
               placeholder="type here"
               value={newName}
-              onChange={handleInput}
+              onChange={(e) => setNewName(e.target.value)}
             />
           </p>
           <p>
@@ -50,7 +58,7 @@ const App = () => {
               type="tel"
               placeholder="+977 98....."
               value={number}
-              onChange={numberHandle}
+              onChange={(e) => setNumber(e.target.value)}
             />
           </p>
         </div>
@@ -59,10 +67,14 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-         {persons.map((person) => {
-        return <p key={person.id}>{person.name} {person.number}</p>;
+      {filteredPerson.map((person) => {
+        return (
+          <p key={person.id}>
+            {person.name} {person.number}
+          </p>
+        );
       })}
-    </div>
+    </>
   );
 };
 
