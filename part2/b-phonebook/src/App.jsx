ae3363d221c173c8f91795f6e56ccacf7806d58d
me-Sushil <girbahadurbk123@gmail.com>
2025-08-09 21:45:31 +0545

    Task 2.7 completed,  change onClick to onSubmit

--- part2/b-phonebook/src/App.jsx
+++ part2/b-phonebook/src/App.jsx
@@ -1,46 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id:1, name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
-  function handleSubmit(event){
-
+  function handleSubmit(event) {
     event.preventDefault();
-    const newObj = {
-      id:persons.length+1,
-      name:newName
+    if (persons.some((person) => person.name === newName.trim())) {
+      window.alert(`${newName} already added to phonebook`);
+      setNewName("");
+    } else {
+      const newObj = {
+        id: persons.length + 1,
+        name: newName,
+      };
+      setPersons(persons.concat(newObj));
+      setNewName("");
     }
-    setPersons(persons.concat(newObj));
-    setNewName("");
   }
-
-  function handleInput(event){
+  function handleInput(event) {
     setNewName(event.target.value);
-    
-
-
   }
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onClick={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input placeholder='type here' value={newName} onChange={handleInput}/>
+          name:
+          <input
+            placeholder="type here"
+            value={newName}
+            onChange={handleInput}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person)=>{
-        return <p key ={person.id}>{person.name}</p>})}
+      {persons.map((person) => {
+        return <p key={person.id}>{person.name}</p>;
+      })}
     </div>
-  )
-
-}
-
-export default App
+  );
+};
+export default App;
