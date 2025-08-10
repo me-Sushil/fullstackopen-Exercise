f58a79e6d7c355d8df17bfeaea645d6749e0bc60
me-Sushil <girbahadurbk123@gmail.com>
2025-08-10 18:52:55 +0545

    Task 2.11  complete

--- part2/b-phonebook/src/App.jsx
+++ part2/b-phonebook/src/App.jsx
@@ -8,17 +8,19 @@ const App = () => {
   const [persons, setPersons] = useState([]);
   const [search, setSearch] = useState("");
 
-  useEffect(()=>{
-    
-  },[])
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
   return (
     <>
       <h2>Phonebook</h2>
-      <Filter search={search} setSearch={setSearch}/>
+      <Filter search={search} setSearch={setSearch} />
       <h3>add a new</h3>
-      <PersonForm persons={persons} setPersons={setPersons}/>
+      <PersonForm persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-      <Person search={search} persons={persons}/>
+      <Person search={search} persons={persons} />
     </>
   );
 };
