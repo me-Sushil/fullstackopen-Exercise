60bf4cb0cd1e74dd5200acc5cab55cdd64e5dbdc
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-20 18:08:19 +0545

    get : data, filter and make condition to display

--- part2/e-countries/src/App.jsx
+++ part2/e-countries/src/App.jsx
@@ -1,13 +1,55 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./style.css";
 function App() {
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState([]);
+  const [filter, setFilter] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all/`)
+      .then((response) => setResult(response.data));
+  }, []);
+
+  console.log(result, "this is result");
+
+  useEffect(() => {
+    setFilter(
+      result.filter((co) =>
+        co.name.common.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, result]);
+
+  console.log(filter, "this is filter data");
 
   return (
     <>
       <div>
-      <p>Find countries <input className="search" placeholder="search country"></input></p>
+        <p>
+          Find countries{" "}
+          <input
+            className="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="search country"
+          ></input>
+        </p>
+        {search &&(filter.length > 10 ? (
+            <p> Too many matches, specify another filter</p>
+          ) : (
+            <div>
+              <ul>
+                {filter.map((co, index) => (
+                  <li key={index}>{co.name.common}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
