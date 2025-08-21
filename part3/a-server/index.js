6cdef35be2cf6877212fd288f5b0a54f3db49293
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-21 16:41:15 +0545

    import express, give port

--- part3/a-server/index.js
+++ part3/a-server/index.js
@@ -1,1 +1,9 @@
-console.log("hello from server");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+
+
+const PORT = 3001;
+app.listen(PORT);
+console.log("The server is running", PORT);
