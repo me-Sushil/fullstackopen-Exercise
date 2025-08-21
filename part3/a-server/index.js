83a68da2a34eec4014bae035460126d5430ef6bf
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-21 17:04:30 +0545

    create /info inpoint and send length of phoonebook

--- part3/a-server/index.js
+++ part3/a-server/index.js
@@ -30,6 +30,13 @@ app.get("/api/persons", (request, response)=>{
     response.json(person);
 })
 
+
+app.get("/info", (request, response)=>{
+response.send(
+   `<p>Phonebook has info for ${person.length} people</p>`
+)
+})
+
 const PORT = 3001;
 app.listen(PORT);
 console.log("The server is running", PORT);
