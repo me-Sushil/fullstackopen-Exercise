14608d6fa5c271c40593335f652dbdc4a80214bf
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-23 23:59:07 +0545

    give option to place port for deployment

--- part3/a-server/index.js
+++ part3/a-server/index.js
@@ -96,5 +96,5 @@ app.post("/api/persons", (request, response) => {
   response.json(newData);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT ? process.env.PORT: 3001;
 app.listen(PORT, () => console.log("The server is running", PORT));
