6b941c7d292a25727db8d4514ae517edb369bcaf
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-24 16:46:57 +0545

    Add:  person added successfully

--- part3/a-server/mongo.js
+++ part3/a-server/mongo.js
@@ -1,36 +1,34 @@
 const { response } = require('express')
 const mongoose = require('mongoose')
 
-if (process.argv.length < 5) {
+if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
-const url = `mongodb+srv://phonebookApp:${password}@cluster0.8gckl7e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+const url = `mongodb+srv://phonebookApp:${password}@cluster0.8gckl7e.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
+const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 })
 
-const Person = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Person({
-  name: process.argv[3],
-  number: process.argv[4],
+const person = new Person({
+  name,
+  number,
 })
 
-note.save().then(result => {
-  console.log('note saved!')
+person.save().then(result => {
+  console.log(`added ${name} number ${number} to phonebook`);
   mongoose.connection.close()
-})
-
-Person.find({}).then((result)=>{
-    response.json(result);
-      mongoose.connection.close();
 })
