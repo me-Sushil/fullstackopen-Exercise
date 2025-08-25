ac142a2d1df73d79f8fa7d559bb6395a3c99127a
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-25 12:45:46 +0545

    Task : 3.17  Completed

--- part3/a-server/index.js
+++ part3/a-server/index.js
@@ -122,9 +122,11 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === "CastError") {
     // Invalid ObjectId format (e.g., malformed MongoDB _id)
     return response.status(400).send({ error: "malformatted id" });
+
   } else if (error.name === "ValidationError") {
     // Mongoose schema validation failed (invalid or missing data)
     return response.status(400).json({ error: error.message });
+    
   } else if (error.name === "MongoServerError" && error.code === 11000) {
     // Unique constraint violation (duplicate value for a field marked as unique)
     return response.status(400).json({ error: "Duplicate field value" });
