4cf043dc15e6411ba976d79ca809f0279a5afe0f
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-25 23:51:54 +0545

    correct: spelling of response

--- part2/b-phonebook/src/services/noteServices.js
+++ part2/b-phonebook/src/services/noteServices.js
@@ -1,20 +1,26 @@
 import axios from "axios";
 
-const url = "/api/persons";
+const url = "http://localhost:3001/api/persons";
 
-const getAll=()=>{
-   return axios.get(url).then(responce=>responce.data);
-}
+const getAll = () => {
+  return axios.get(url).then((response) => response.data);
+};
 
-const create=(newObj)=>{
-   return axios.post(url, newObj).then(responce=>responce.data)
-}
-const deleteNote=(id)=>{
-    return axios.delete(`${url}/${id}`).then(response=> response.data);
-}
+const create = (newObj) => {
+  return axios
+    .post(url, newObj)
+    .then((response) => response.data)
+    .catch((error) => {
+        if(error.response)
+        error.response.data
+    });
+};
+const deleteNote = (id) => {
+  return axios.delete(`${url}/${id}`).then((response) => response.data);
+};
 
-const update=(id, newObj)=>{
-    return axios.put(`${url}/${id}`, newObj).then(responce=>responce.data);
-}
+const update = (id, newObj) => {
+  return axios.put(`${url}/${id}`, newObj).then((response) => response.data);
+};
 
-export default {getAll, create, deleteNote, update};
+export default { getAll, create, deleteNote, update };
