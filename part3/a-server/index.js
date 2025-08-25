da82a79fad46cbbdcc5725c4fce6aa81f1f9cd08
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-25 11:42:34 +0545

    Task 3.16 Completed

--- part3/a-server/index.js
+++ part3/a-server/index.js
@@ -19,16 +19,17 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
   Person.find({})
     .then((result) => {
       response.json(result);
     })
     .catch((error) => {
-      console.error(error.message);
-      response
-        .status(500)
-        .send({ error: "something went wrong with database" });
+      next(error);
+      // console.error(error.message);
+      // response
+      //   .status(500)
+      //   .send({ error: "something went wrong with database" });
     });
 });
 
@@ -41,31 +42,34 @@ app.get("/api/persons", (request, response) => {
 //   );
 // });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const personId = request.params.id;
   Person.findById(personId)
     .then((person) => {
       response.json(person);
     })
-    .catch((err) => response.status(404).json("Data not found", err));
+    .catch((error) => {
+      next(error);
+      // response.status(404).json("Data not found", err)
+    });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const perId = request.params.id;
   Person.findByIdAndDelete(perId)
     .then((result) => response.status(204).end())
-    .catch((err) => err);
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const data = request.body;
 
-  if (!data.name) {
-    return response.status(400).send({ error: "name is missing" });
-  }
-  if (!data.number) {
-    return response.status(400).send({ error: "number is missing" });
-  }
+  // if (!data.name) {
+  //   return response.status(400).send({ error: "name is missing" });
+  // }
+  // if (!data.number) {
+  //   return response.status(400).send({ error: "number is missing" });
+  // }
   Person.findOne({ name: data.name }).then((existingPerson) => {
     if (existingPerson) {
       return response.status(400).json({ error: "name must be unique" });
@@ -83,9 +87,31 @@ app.post("/api/persons", (request, response) => {
       response.json(savedPerson);
     })
     .catch((error) => {
-      console.log("Error to post data", error);
+      next(error);
+      // console.log("Error to post data", error);
     });
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    // Invalid ObjectId format (e.g., malformed MongoDB _id)
+    return response.status(400).send({ error: "malformatted id" });
+
+  } else if (error.name === "ValidationError") {
+  // Mongoose schema validation failed (invalid or missing data)
+    return response.status(400).json({ error: error.message });
+    
+  } else if (error.name === "MongoServerError" && error.code === 11000) {
+  // Unique constraint violation (duplicate value for a field marked as unique)
+    return response.status(400).json({ error: "Duplicate field value" });
+  }
+ 
+  next(error);
+};
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () =>
   console.log("The server is running", process.env.PORT)
