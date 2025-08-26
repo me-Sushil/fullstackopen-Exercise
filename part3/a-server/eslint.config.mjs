90d21bb3584fb53c249f5fce8267396d188c810f
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-27 00:19:52 +0545

    Task 3.22 completed, done all setup of eslint

--- part3/a-server/eslint.config.mjs
+++ part3/a-server/eslint.config.mjs
@@ -17,8 +17,8 @@ export default [
     rules: {
       '@stylistic/js/indent': ['error', 2],
       '@stylistic/js/linebreak-style': ['error', 'unix'],
-      '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'never'],
+      '@stylistic/js/quotes': ['error', 'double'],
+      '@stylistic/js/semi': ['error', 'always'],
       eqeqeq: 'error',
       'no-trailing-spaces': 'error',
       'object-curly-spacing': ['error', 'always'],
