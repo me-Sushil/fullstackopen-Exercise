bd41c1432538c725ce8b576d7cbcc940f488a665
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-31 22:55:28 +0545

    database  : connection log

--- part4/app.js
+++ part4/app.js
@@ -1,16 +1,22 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
 const config = require("./utils/config");
 const blogRouter = require("./controllers/blogs");
 const middleWare = require("./utils/middleware");
-require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(config.MONGODB_URI);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then((result) => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.log("Error on connection database");
+  });
 
 app.use(middleWare.requestLogger);
 app.use("/api/blogs", blogRouter);
