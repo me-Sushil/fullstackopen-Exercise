102bcb4e4d1c0939963d6e58cc9bf7a4ab2346b8
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-31 19:07:21 +0545

    Get default code from fullStackOpen

--- part4/index.js
+++ part4/index.js
@@ -4,6 +4,39 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+app.use(express.json());
+app.use(cors());
+
+
+
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl)
+
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
 
 app.listen(process.env.PORT, () => {
   console.log(`The server is running on PORT ${process.env.PORT}`);
