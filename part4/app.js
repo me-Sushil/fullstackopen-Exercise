56b16b7971641da208e390bc6d0aa80d1c35e9c0
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-31 22:50:14 +0545

    import : middleware and use

--- part4/app.js
+++ part4/app.js
@@ -3,30 +3,18 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const config = require("./utils/config");
-
+const blogRouter = require("./controllers/blogs");
+const middleWare = require("./utils/middleware");
 require("dotenv").config();
-
 
 app.use(express.json());
 app.use(cors());
 
-
-
 mongoose.connect(config.MONGODB_URI);
 
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+app.use(middleWare.requestLogger);
+app.use("/api/blogs", blogRouter);
+app.use(middleWare.errorhandler);
+app.use(middleWare.unknownEndpoint);
 
 module.exports = app;
