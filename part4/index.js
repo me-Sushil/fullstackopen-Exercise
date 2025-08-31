e2441de831ae2710a508fd8a7042c4451433fd9d
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-08-31 22:06:32 +0545

    create : app file

--- part4/index.js
+++ part4/index.js
@@ -1,43 +1,5 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const cors = require("cors");
+const app = require("./app");
 
-require("dotenv").config();
-
-
-const MONGODB_URI = process.env.MONGODB_URI;
-app.use(express.json());
-app.use(cors());
-
-
-
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(MONGODB_URI);
-
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 
 app.listen(process.env.PORT, () => {
