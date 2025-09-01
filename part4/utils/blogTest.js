2d2dc7bc1a7e7c065c8aba481f108d63178e1cca
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-01 14:07:54 +0545

    Write : mostBlogs function

--- part4/utils/blogTest.js
+++ part4/utils/blogTest.js
@@ -1,31 +1,47 @@
 const dummy = (blogs) => {
   return 1;
-}
+};
 
-const totalLikes=(blogs)=>{
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, blog) => {
+    return (acc += blog.likes);
+  }, 0);
+};
 
-    return blogs.reduce((acc, blog)=>{
-       return acc+= blog.likes;
-    },0)
-}
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+  return blogs.reduce((favorite, blog) => {
+    return blog.likes > favorite.likes ? blog : favorite;
+  });
+};
 
-const favoriteBlog =(blogs)=>{
-    if(blogs.length === 0) return null;
-   return blogs.reduce((favorite, blog)=>{
-       return blog.likes > favorite.likes ? blog : favorite;
-    })
-}
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null;
 
-const mostBlogs  =(blogs)=>{
-    if(blogs.length === 0) return null;
+  const blogcounterByAuthor = {};
 
-    
+  blogs.forEach((blog) => {
+    blogcounterByAuthor[blog.author] =
+      (blogcounterByAuthor[blog.author] || 0) + 1;
+  });
 
-}
+  let topAuthor = null;
+  let maxBlogs = 0;
+  for (let author in blogcounterByAuthor) {
+    if (blogcounterByAuthor[author] > maxBlogs) {
+        topAuthor = author;
+        maxBlogs = blogcounterByAuthor[author];
+    }
+    return {
+      author: topAuthor,
+      blogs: maxBlogs,
+    };
+  }
+};
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs 
-}
+  mostBlogs,
+};
