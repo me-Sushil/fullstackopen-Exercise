6bb1012f56110f0c6c1cf1c0f028fe438d92e72a
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-01 11:39:32 +0545

    Add:  favoriteBlog function

--- part4/utils/blogTest.js
+++ part4/utils/blogTest.js
@@ -3,13 +3,21 @@ const dummy = (blogs) => {
 }
 
 const totalLikes=(blogs)=>{
-    
+
     return blogs.reduce((acc, blog)=>{
        return acc+= blog.likes;
     },0)
 }
 
+const favoriteBlog =(blogs)=>{
+    if(blogs.length === 0) return null;
+    blogs.reduce((favorite, blog)=>{
+       return blog.likes > favorite.likes ? blog : favorite;
+    })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
