5e9818cb80051f3f68877ed186105b6144515731
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-03 17:14:01 +0545

    Test :  valid blog test pass

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,19 +1,25 @@
 const { test, after } = require("node:test");
+const assert = require("node:assert");
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 const app = require("../app");
 
 const api = supertest(app);
 
-test.only("blogs are return as json", async () => {
+test("blogs are return as json", async () => {
   await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
 });
+test("all blogs are returned", async () => {
+  const response = await api.get("/api/blogs");
+
+  assert.strictEqual(response.body.length, 3);
+});
 
 test("blogs have field named id instead of _id", async()=>{
-     const response = await api.get('/api/blogs').expect(200)
+     const response = await api.get("/api/blogs").expect(200)
 
   const blogs = response.body
   blogs.forEach(blog => {
@@ -22,6 +28,17 @@ test("blogs have field named id instead of _id", async()=>{
   })
 })
 
+test("A valid blog can be added", async()=>{
+    const newBlog = {
+    id: "5dw22a851b54a676234d17f7",
+    title: "React grandchild",
+    author: "React ok Chan",
+    url: "https://reactgrandchild.com/",
+    likes: 12
+    }
+
+    await api.post("/api/blogs").send(newBlog).expect(201).expect("Content-Type", /application\/json/)
+})
 after(async()=>{
     await mongoose.connection.close();
 })
