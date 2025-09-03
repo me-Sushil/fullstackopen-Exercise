ea85df1580eddaeb35bfb7ec44760e9c34819b6e
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-03 17:43:36 +0545

    Task :  4.10 Completed

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -10,22 +10,22 @@ blogRouter.get("/", async (request, response, next) => {
   }
 });
 
-blogRouter.post("/", (request, response, next) => {
-  const { title, author, url, likes } = request.body;
+blogRouter.post("/", async (request, response, next) => {
+  try {
+    const { title, author, url, likes } = request.body;
 
-   if (!title) {
+    if (!title) {
       return response.status(400).json({
         error: "content is missing",
       });
     }
-  const blog = new Blog({ title, author, url, likes });
+    const blog = new Blog({ title, author, url, likes });
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((error) => next(error));
+    const result = await blog.save();
+    response.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = blogRouter;
