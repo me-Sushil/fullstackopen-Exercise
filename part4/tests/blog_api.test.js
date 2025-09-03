23007d452463acaefe189cc1a5b31110c060d637
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-03 12:31:41 +0545

    Task 4.9   completed

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -12,6 +12,16 @@ test.only("blogs are return as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
+test("blogs have field named id instead of _id", async()=>{
+     const response = await api.get('/api/blogs').expect(200)
+
+  const blogs = response.body
+  blogs.forEach(blog => {
+    assert.ok(blog.id, 'Blog has id field')
+    assert.strictEqual(blog._id, undefined)
+  })
+})
+
 after(async()=>{
     await mongoose.connection.close();
 })
