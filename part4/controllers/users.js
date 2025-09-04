95343fdf2ec28e198c66d280934570495281782f
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-05 02:18:29 +0545

    Task : 4. 16 Complete ,  without test

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -3,15 +3,32 @@ const User = require("../models/user");
 const userRouter = require("express").Router();
 const bcrypt = require("bcrypt");
 
-
-
 userRouter.post("/", async (request, response, next) => {
   try {
     const { username, name, password } = request.body;
     console.log(password, " this is password");
+    // if(!(username && password)){
+    //     return response.status(400).json({error: "Invalid username and password"});
+    // }
+    if (!username || username.length < 3) {
+      return response
+        .status(400)
+        .json({ error: "Username must be at least 3 characters long" });
+    }
 
-     const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(password, saltRounds)
+    if (!password || password.length < 3) {
+      return response
+        .status(400)
+        .json({ error: "Password must be at least 3 characters long" });
+    }
+
+    const isExist = await User.findOne({ username });
+    if (isExist) {
+      return response.status(400).json({ error: "username must be unique" });
+    }
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
     console.log(passwordHash, " password hash");
     const user = new User({ username, name, passwordHash });
@@ -26,14 +43,13 @@ userRouter.post("/", async (request, response, next) => {
   }
 });
 
+userRouter.get("/", async (request, response, next) => {
+  try {
+    const result = await User.find({});
 
-userRouter.get("/", async(request, response, next)=>{
-    try{
-        const result = await User.find({});
-
-        response.status(200).json(result);
-    }catch(error){
-        next(error);
-    }
-})
+    response.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = userRouter;
--- part4/models/user.js
+++ part4/models/user.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema({
   username: String,
   name: String,
   passwordHash: String,
-  
 });
 
 userSchema.set("toJSON", {
