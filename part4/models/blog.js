ac8512c62d49e5e676a50cba9a5eea631f66b7d8
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-05 02:25:05 +0545

    Add : user reference

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-
 mongoose.set("strictQuery", false);
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: String,
   url: { type: String, required: true },
-  likes: { type: Number, default: 0 }
+  likes: { type: Number, default: 0 },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 blogSchema.set("toJSON", {
@@ -18,6 +21,6 @@ blogSchema.set("toJSON", {
   },
 });
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
 module.exports = Blog;
