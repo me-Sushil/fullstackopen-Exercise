8f0d020779b5e4ba0bdce6feff5faae5fd9a4aa7
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-05 00:55:02 +0545

    Hash the password

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -1,22 +1,25 @@
 const User = require("../models/user");
 const userRouter = require("express").Router();
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
-userRouter.post("/", async(request, response, next)=>{
-    try{
-        const {username, password, name } = request.body;
+userRouter.post("/", async (request, response, next) => {
+  try {
+    const { username, password, name } = request.body;
 
-        const saltRound = 10;
-        const passwordHash = await bcrypt.hash(password, saltRound)
+    const saltRound = 10;
+    const passwordHash = await bcrypt.hash(password, saltRound);
 
-        const user = new User({username, passwordHash, name });
+    console.log(passwordHash, " password hash");
+    const user = new User({ username, passwordHash, name });
 
-        const savedUser = await user.save();
+    const savedUser = await user.save();
 
-        response.status(201).json(savedUser);
+    response.status(201).json(savedUser);
 
+    console.log(savedUser, "this is saved user");
+  } catch (error) {
+    next(error);
+  }
+});
 
-    }catch(error){
-        next(error);
-    }
-})
+module.exports = userRouter;
