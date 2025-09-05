15cd04e51bf854e731d07428fbadab776e9fc67b
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-05 14:57:19 +0545

    Add :  validate username, verify password, and generate token

---
+++ part4/controllers/login.js
@@ -0,0 +1,31 @@
+const loginRouter = require("express").Router();
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+loginRouter.post("/", async (request, response, next) => {
+  const { username, password } = request.body;
+
+  const user = await User.findOne({ username });
+  console.log(user, "user");
+
+  const isValidPassword =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+
+  if (!(isValidPassword && user)) {
+    return response
+      .status(401)
+      .json({ error: "Invalide username or password" });
+  }
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  };
+
+  const token = jwt.sign(userForToken, "the sekret key")
+  console.log(token);
+
+  response.status(200).json({token:token, username:user.username, name:user.name});
+});
+
+module.exports = loginRouter;
