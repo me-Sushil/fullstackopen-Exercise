999361581e83a8cd53cbef98c91300327c2e4cdf
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-05 17:02:55 +0545

    create :  tokenExtractor middleware

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -39,9 +39,18 @@ const errorhandler = (error, request, response, next) => {
   next(error);
 };
 
-
+const tokenExtractor =(request, response, next)=>{
+  const authorization = request.get("authorization");
+  if(authorization && authorization.startsWith("Bearer ")){
+    request.token = authorization.replace("Bearer ", "");
+  }else{
+      request.token = null;
+  }
+  next();
+}
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorhandler
+  errorhandler,
+  tokenExtractor
 }
