45f6d2002410e8bbb51955c3f90e1eb610c26f1e
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-05 15:04:38 +0545

    import sekret and export

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 const MONGODB_URI = process.env.NODE_ENV === "test" ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
 const PORT = process.env.PORT ? process.env.PORT : 3001;
-
-module.exports = {MONGODB_URI, PORT};
+const SEKRET = process.env.SEKRET;
+module.exports = {MONGODB_URI, PORT, SEKRET};
