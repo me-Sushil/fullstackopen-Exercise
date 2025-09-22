0ec7524a72ec718ee32443f05a2d51b9d2679049
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-09-22 17:25:08 +0545

    Task 5.1  Completed

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -5,75 +5,72 @@ import blogService from "./services/blogs";
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [user, setUser] = useState(null);
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
-   const handleSubmit = async (e) => {
-      e.preventDefault();
-      const userData = {
-        username,
-        password,
-      };
-      try {
-        const result = await blogService.login(userData);
-        setUser(result);
-        setUsername("");
-        setPassword("");
-      } catch (error) {
-        console.log(error,"login failed error");
-      }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const userData = {
+      username,
+      password,
     };
+    try {
+      const result = await blogService.login(userData);
+      setUser(result);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      console.log(error, "login failed error");
+    }
+  };
 
-  const loginForm = () => 
-    (
-      <>
-        <h2>log in to Application</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>
-              username
-              <input
-                value={username}
-                type="text"
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Enter username"
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              password
-              <input
-                value={password}
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter password"
-              />
-            </label>
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </>
-    );
-  
+  const loginForm = () => (
+    <>
+      <h2>log in to Application</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            username
+            <input
+              value={username}
+              type="text"
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter username"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              value={password}
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter password"
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
+  );
 
-  const blogForm=()=>(
-      <div>
-        <h2>blogs</h2>
-        {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} />
-        ))}
-      </div>
-    )
-  
+  const blogForm = () => (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
 
   return (
     <>
-     {!user && loginForm()} 
+      {!user && loginForm()}
       {user && blogForm()}
     </>
   );
