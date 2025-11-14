487d8b624d06d13dba0fb6d697ac969cd5698577
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-14 15:17:50 +0545

    create new blog services and assign token

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,22 @@
 import axios from "axios";
 const baseUrl = "/api/blogs";
 
+let token = null;
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
+
 const getAll = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 };
+const postBlog = async (newBlog) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.post(baseUrl, newBlog, config);
+  return response.data;
+};
 
-export default { getAll };
+export default { getAll, setToken, postBlog };
