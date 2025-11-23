bc2e24ac714fdf1dc47e2c9b2759a7f5b05cfc60
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-23 12:18:16 +0545

    Add : all details

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,26 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
-
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button>view</button>
+      </div>
+      <div>
+        {blog.url}
+      </div>
+      <div>
+       likes {blog.likes}<button>like</button>
+      </div>
+      <div>
+        {blog.user.name}
+      </div>
+    </div>
+  );
+};
+export default Blog;
