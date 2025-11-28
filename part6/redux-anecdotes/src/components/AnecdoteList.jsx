c96acc7c9ae5ce6980ae632b7e651fe2c655eabf
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-28 17:10:08 +0545

    6.11 Completed

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import { vote } from "../reducers/anecdoteReducer";
+import { votes } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(({ filter, anecdotes }) => {
@@ -17,10 +17,9 @@ const AnecdoteList = () => {
         .sort((a, b) => b.votes - a.votes)
         .map((anecdote) => (
           <div key={anecdote.id}>
-            <div>{anecdote.content}</div>
             <div>
-              has {anecdote.votes}
-              <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+              {anecdote.content} has {anecdote.votes}
+              <button onClick={() => dispatch(votes(anecdote.id))}>vote</button>
             </div>
           </div>
         ))}
