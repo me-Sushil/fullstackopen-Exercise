4d1f1814d4f023a2696fd1fdfbb32fefce4b31c4
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-11-30 13:37:56 +0545

    fetch all anecdote at first render

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,15 +2,23 @@ import AnecdoteList from "./components/AnecdoteList";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { useEffect } from "react";
+import { getAll } from "./services/anecdote";
+import { useDispatch } from "react-redux";
+import { allAnecdotes } from "./reducers/anecdoteReducer";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getAll().then((data) => dispatch(allAnecdotes(data)));
+  }, []);
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Notification/>
-      <Filter/>
-      <AnecdoteList/>
-      <AnecdoteForm/>
+      <Notification />
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   );
 };
