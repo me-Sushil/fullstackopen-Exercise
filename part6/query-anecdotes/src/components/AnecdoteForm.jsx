67d310116d3211a3ac2e81f26017950d32bc2b30
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-01 01:50:08 +0545

    send new anecdptes using react query with optimization

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,23 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdotes } from '../services/requests'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdotesMutation = useMutation({
+    mutationFn: createAnecdotes,
+    onSuccess: (newAnecdotes) => {
+      const anecnotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecnotes.concat(newAnecdotes))
+    },
+  })
+
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
+    newAnecdotesMutation.mutate({ content, votes: 0 })
   }
 
   return (
