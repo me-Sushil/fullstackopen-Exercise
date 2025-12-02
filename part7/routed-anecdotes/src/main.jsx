57da256870c11b0b32bcbe566849febb1e08d90a
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-02 15:07:31 +0545

    Wrape App with browserRouter

--- part7/routed-anecdotes/src/main.jsx
+++ part7/routed-anecdotes/src/main.jsx
@@ -1,4 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
