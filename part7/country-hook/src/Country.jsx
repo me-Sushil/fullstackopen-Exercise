e3ea92c7a25b1c6772af8283270c8509a53d4f64
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-03 14:14:37 +0545

    Distructure the country component

--- part7/country-hook/src/Country.jsx
+++ part7/country-hook/src/Country.jsx
@@ -1,23 +1,30 @@
 const Country = ({ country }) => {
+  // country.name is an object with 'common' and 'official' properties, not a string
+  console.log("all details of country", country?.name?.common);
   if (!country) {
-    return null
+    return null;
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>;
   }
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      {/* country.name is an object, use country.name.common for the display name */}
+      <h3>{country.name.common}</h3>
+      {/* country.capital is an array, use [0] to get the first element */}
+      <div>capital {country.capital[0]}</div>
+      {/* country.population is a number */}
+      <div>population {country.population}</div>
+      {/* country.flags is an object with 'svg' and 'png' properties, use .svg for the image source */}
+      <img
+        src={country.flags.svg}
+        height="100"
+        alt={`flag of ${country.name.common}`}
+      />
     </div>
-  )
-}
+  );
+};
+
 export default Country;
