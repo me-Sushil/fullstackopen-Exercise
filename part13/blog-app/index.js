77f8d29d2db82b22103bcfda56d1690863a1b270
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-04 21:26:24 +0545

    create table, get request, and post request

--- part13/blog-app/index.js
+++ part13/blog-app/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const express = require('express')
-const app = express()
-app.use(express.json())
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const express = require("express");
+const app = express();
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -12,12 +12,52 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
   },
 });
+class Blog extends Model {}
 
-app.get('/api/notes', async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
-  res.json(notes)
-})
-const PORT = process.env.PORT || 3001
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
+  }
+);
+
+Blog.sync();
+
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+app.post("/api/blogs", async (req, res) => {
+  const blog = await Blog.create(req.body);
+  res.json(blog);
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
