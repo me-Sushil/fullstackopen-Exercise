1f5bf451d061b359b15a05a0e4c6cc4b93041a5a
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-04 22:03:31 +0545

    13.4 Completed

--- part13/blog-app/index.js
+++ part13/blog-app/index.js
@@ -4,58 +4,70 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
-// class Blog extends Model {}
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+class Blog extends Model {}
 
-// Blog.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     author: {
-//       type: DataTypes.TEXT,
-//     },
-//     url: {
-//       type: DataTypes.TEXT,
-//       allowNull: false,
-//     },
-//     title: {
-//       type: DataTypes.TEXT,
-//       allowNull: false,
-//     },
-//     likes: {
-//       type: DataTypes.INTEGER,
-//       defaultValue: 0,
-//     },
-//   },
-//   {
-//     sequelize,
-//     underscored: true,
-//     timestamps: false,
-//     modelName: "blog",
-//   }
-// );
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
+  }
+);
 
-// Blog.sync();
+Blog.sync();
 
-// app.get("/api/blogs", async (req, res) => {
-//   const blogs = await Blog.findAll();
-//   res.json(blogs);
-// });
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
 
-// app.post("/api/blogs", async (req, res) => {
-//   const blog = await Blog.create(req.body);
-//   res.json(blog);
-// });
+app.post("/api/blogs", async (req, res) => {
+  const blog = await Blog.create(req.body);
+  res.json(blog);
+});
+
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const deleted = await Blog.destroy({
+    where: { id: req.params.id }
+  });
+  if (!deleted) {
+    return res.status(404).json({ error: "Blog not found" });
+  }
+  res.status(204).end();
+});
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
