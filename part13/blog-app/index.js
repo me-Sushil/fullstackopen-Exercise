32853203b66b30d991e0aeddf2b0b1cd29c9530e
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-06 14:57:12 +0545

    13.7 Completed

--- part13/blog-app/index.js
+++ part13/blog-app/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const middleware = require("./util/middleware");
 const { PORT } = require("./util/config");
 const { connectToDatabase } = require("./util/db");
 const blogsRouter = require("./controllers/blogs");
 
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.errorhandler);
 
 const start = async () => {
   await connectToDatabase();
