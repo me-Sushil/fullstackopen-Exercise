adb7602dd14b08c5c4667738fa2e9734713795a8
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-06 16:02:55 +0545

    13.10  completed ,

--- part13/blog-app/controllers/login.js
+++ part13/blog-app/controllers/login.js
@@ -16,6 +16,7 @@ router.post("/", async (request, response, next) => {
     const passwordCorrect = body.password === "secret";
 
     if (!(user && passwordCorrect)) {
+        
       return response.status(401).json({
         error: "invalid username or password",
       });
