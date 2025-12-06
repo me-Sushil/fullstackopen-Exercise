6877b2e0a0cba79bac489ad73fa59fc839258399
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-06 16:02:32 +0545

    create login endpoint and create token

---
+++ part13/blog-app/controllers/login.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const router = require("express").Router();
+
+const { SECRET } = require("../util/config");
+const User = require("../models/user");
+
+router.post("/", async (request, response, next) => {
+  const body = request.body;
+  try {
+    const user = await User.findOne({
+      where: {
+        username: body.username,
+      },
+    });
+
+    const passwordCorrect = body.password === "secret";
+
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: "invalid username or password",
+      });
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user.id,
+    };
+
+    const token = jwt.sign(userForToken, SECRET);
+
+    response
+      .status(200)
+      .send({ token, username: user.username, name: user.name });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
