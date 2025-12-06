5cf696cbd4d62c5d5dea40def5cceff589944f4b
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-06 15:33:15 +0545

    13.8 COmpleted

--- part13/blog-app/controllers/users.js
+++ part13/blog-app/controllers/users.js
@@ -13,4 +13,32 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:username", async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+    where: { username: req.params.username }
+  });
+   if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    // Update allowed fields
+    if (req.body.username) {
+      user.username = req.body.username;
+    }
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
