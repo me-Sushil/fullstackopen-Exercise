fa3eccad5bf533b02b6dadab45c553e63e51663a
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-08 17:59:07 +0545

    Import readRouter

--- part13/blog-app/index.js
+++ part13/blog-app/index.js
@@ -8,14 +8,15 @@ const { connectToDatabase } = require("./util/db");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
-const authorsRouter = require('./controllers/authors')
-
+const authorsRouter = require("./controllers/authors");
+const readRouter = require("./controllers/readList");
 
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
-app.use('/api/authors', authorsRouter)
+app.use("/api/authors", authorsRouter);
+app.use("/api/readinglists", readRouter);
 
 app.use(middleware.errorhandler);
 
