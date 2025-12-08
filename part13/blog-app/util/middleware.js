5c3839416bbd99ba0fbf883904bca8731e373604
Sushil Bishowkarma <girbahadurbk123@gmail.com>
2025-12-08 18:02:15 +0545

    separate token extractor

--- part13/blog-app/util/middleware.js
+++ part13/blog-app/util/middleware.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("./config");
+
 const errorhandler = (error, req, res, next) => {
   console.error(error.message);
 
@@ -5,10 +8,10 @@ const errorhandler = (error, req, res, next) => {
     return res.status(400).json({ error: error.message });
   }
 
-    // Sequelize validation error  <-- EMAIL validation will come here
+  // Sequelize validation error  <-- EMAIL validation will come here
   if (error.name === "SequelizeValidationError") {
     return res.status(400).json({
-      error: error.errors.map(e => e.message)
+      error: error.errors.map((e) => e.message),
     });
   }
   // Sequelize unique constraint error
@@ -23,6 +26,25 @@ const errorhandler = (error, req, res, next) => {
   // Default fallback
   return res.status(500).json({ error: "Something went wrong" });
 };
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  console.log(authorization, "this is authorization");
+
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      console.log(authorization.substring(7));
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" });
+  }
+  next();
+};
 module.exports = {
   errorhandler,
+  tokenExtractor,
 };
